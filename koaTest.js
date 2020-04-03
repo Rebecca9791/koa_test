2ac8992e2b7561ee625de450902c4b3ee919238c
SunMoon <798063071@qq.com>
2020-04-04 01:13:56 +0800

    连接3001，仍有报错

--- koaTest.js
+++ koaTest.js
@@ -1,13 +1,28 @@
-const http = require('http');
-const https = require('https');
+const http = require('http')
 const Koa = require('koa');
+const io = require('socket.io')(http);
 const app = new Koa();
-
+const app2 = new Koa();
+const net = require('net')
+app2.use(async ctx => {
+  ctx.body = 'hi,3001'
+  console.log(3001)
+}).listen(3001)
 app.use(async ctx => {
-  ctx.body = 'Hi girl';
-  console.log(ctx)
+  ctx.body = 'hi,3000'
+  // const socketWeb = 'http://localhost:3001/';
+  io.sockets.on('connection', socket => {
+    // io.sockets.emit('connect', { 'status': '正确' });
+    console.log('connected')
+  });
+  const client = new net.Socket()
+  try {
+    client.connect('http://localhost:3001/', () => {
+      console.log('天')
+    })
+  } catch{
+    client.destroy()
+  }
 });
 
-// app.listen(3000);
-http.createServer(app.callback()).listen(3000);
-https.createServer(app.callback()).listen(3001);
+app.listen(3000);
